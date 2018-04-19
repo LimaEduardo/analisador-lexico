c2454d24b16d9d2c5d578a98dc96cd2db44aab97
gabriel-gro <gabriel.ribolive@gmail.com>
2018-04-18 22:06:20 -0300

    testes do analisados de lexemas

--- analisadorLexico.py
+++ analisadorLexico.py
@@ -9,38 +9,180 @@ class AnalisadorLexico:
     def __init__(self, nomeArquivo):
         self.arquivo = LeitorDeArquivos(nomeArquivo)
         self.arquivoLinhas = arquivo.linhasArquivo
-        self.buffer = []
         self.fluxoDeTokens = []
         self.tabelaDeSimbolos = {}
         self.operador = {"=": TipoToken.OPIgual , "==" : TipoToken.OPRecebe, ">": TipoToken.OPMaior, "++" : TipoToken.OPIncrementa, "&&" : TipoToken.OPAnd, "<=" : TipoToken.OPMenorIgual, "!" : TipoToken.OPNao, "-" : TipoToken.OPMenos, "--" : TipoToken.OPDecrementa, "+" : TipoToken.OPSoma, "+=" : TipoToken.OPSomaERecebe, "*": TipoToken.OpMultiplica }
         self.separador = {",": TipoToken.SepVirgula, "." : TipoToken.SepPonto, "[" : TipoToken.SepAbreColchete, "{" : TipoToken.SepAbreChave, "(" : TipoToken.SepAbreParentese, ")" : TipoToken.SepFechaParentese,"}" : TipoToken.SepFechaChave, "]" : TipoToken.SepFechaColchete, ";" : TipoToken.SepPontoEVirgula}
         self.reservada = {"abstract": TipoToken.PCAbstract, "boolean" : TipoToken.PCBoolean, "char" : TipoToken.PCChar, "class" : TipoToken.PCClass, "else" : TipoToken.PCElse ,"extends" : TipoToken.PCExtends ,"false" : TipoToken.PCFalse, "import" : TipoToken.PCImport, "if": TipoToken.PCIf ,"instanceof" : TipoToken.PCInstanceOf, "int" : TipoToken.PCInt, "new" : TipoToken.PCNew, "null" : TipoToken.PCNull, "package" : TipoToken.PCPackage, "private" : TipoToken.PCPrivate, "protected": TipoToken.PCProtected ,"public" : TipoToken.PCPublic, "return" : TipoToken.PCReturn, "static" : TipoToken.PCStatic, "super" : TipoToken.PCSuper, "this" : TipoToken.PCThis, "true" : TipoToken.PCTrue, "void" : TipoToken.PCVoid, "while" : TipoToken.PCWhile}
         self.comentario = False
-        self.string = False        
+        self.string = False
+    
+    
+    def ehCharIdentificador(self,c):
+        if ((ord('a') <= ord(c)) and (ord(c) <= ord('z')) or
+           ((ord('A') <= ord(c)) and (ord(c) <= ord('Z')) or
+           c == '_' or c == '$':
+            return True
+        else
+            return False
+            
+    def ehNumero(self,c):
+        return c.isdigit():
+    
+    def ehOperador(self, c, c1):
+        if c == '=':
+            if c1 == '=':
+                return True
+        elif c == '+':
+            if c1 == '+':
+                return True
+        elif c == '&':
+            if c1 == '&':
+                return True
+        elif c == '<':
+            if c1 == '=':
+                return True
+        elif c == '-':
+            if c1 == '-':
+                return True
+        elif c == '+':
+            if c1 == '=':
+                return True
+        elif c in self.operador:
+            return True
+        else
+            return False
     
     def analisa(self):
-        c = self.arquivo.leProximoChar()
+        ehIdentificador = False
+        ehIntLiteral = False
+        ehStringLiteral = False
+        ehSeparador = False
+        ehOperadorUni = False
+        ehOperadorDup = False
+        ehPalavraChave = False
+        
+        iniLexema = 0
+        indiceLinha = 0
+        while (indiceLinha < len(self.arquivoLinhas)):
+            indiceColuna = 0
+            while (indiceColuna < len(self.arquivoLinhas[indiceLinha])):
+                indiceColuna = 0
+                caractere = self.arquivoLinhas[indiceLinha][indiceColuna]
+                
+                #-------------------------------------------------------------
+                # Testa se é um char Literal
+                if not ehCharLiteral and caractere == '\'':
+                    c1 = self.arquivoLinhas[indiceLinha][indiceColuna + 1]       
+                    if c1 == '\\':
+                        c2 = self.arquivoLinhas[indiceLinha][indiceColuna + 2]
+                        if  c2 == 't' or c2 == 'b' or c2 == 'f' or c2 == '\'' or c2 == '\"' or c2 == '\\' :
+                            if self.arquivoLinhas[indiceLinha][indiceColuna + 3] == '\'':
+                                self.resolve(token("char_literal", caractere+c1+c2+c3)
+                                indiceColuna += 3
+                                continue
+                    elif c1 != '\'' and c1 != '/':
+                        c2 = self.arquivoLinhas[indiceLinha][indiceColuna + 2]
+                        if c2 == '\'':
+                            self.resolve(token("char_literal", caractere+c1+c2)
+                            indiceColuna += 2
+                            continue
+                #-------------------------------------------------------------
+                
+                
+                
+                #-------------------------------------------------------------
+                # Teste se achou um identificador
+                if ehCharIdentificador(caractere):
+                    iniLexema = indiceColuna
+                    while ((c != " ") and (not ehOperador(c)) and (c not in self.separador) and (c != "\t")):
+                        indiceColuna += 1
+                    lexema = self.arquivoLinhas[indiceLinha][iniLexema: indiceColuna]
+                    if lexema not in self.reservada
+                        self.resolve(token("Variavel", lexema))  
+                    else
+                        self.resolve(self.operador[lexema])
+                #-------------------------------------------------------------
+                
+                #-------------------------------------------------------------
+                #Teste se é um numero
+                if ehNumero(caractere):
+                    iniLexema = indiceColuna
+                    while ((c != " ") and (not ehOperador(c)) and (c not in self.separador) and (c != "\t")):
+                        indiceColuna += 1
+                    lexema = self.arquivoLinhas[indiceLinha][iniLexema: indiceColuna]
+                    self.resolve(token("Int", lexema)
+                #-------------------------------------------------------------
+
+
+
+                #-------------------------------------------------------------
+                #Testa se caracter é um operador
+                c1 = self.arquivoLinhas[indiceLinha][indiceColuna + 1]
+                if caractere == '=':
+                    if  == '=':
+                        ehOperadorDup = True
+                elif caractere == '+':
+                    if c1 == '+':
+                        ehOperadorDup = True
+                elif caractere == '&':
+                    if c1 == '&':
+                        ehOperadorDup = True
+                elif caractere == '<':
+                    if c1 == '=':
+                        ehOperadorDup = True
+                elif caractere == '-':
+                    if c1 == '-':
+                        ehOperadorDup = True
+                elif caractere == '+':
+                    if c1 == '=':
+                        ehOperadorDup = True
+                elif caractere in self.operador:
+                    ehOperadorUni = True
+                    ehOperadorDup = False
+                #Criando os tokens de um operador
+                if ehOperadorUni:
+                    self.resolve(self.operador[caracter])
+                    indiceColuna += 1
+                    continue
+                elif ehOperadorDup:
+                    self.resolve(self.operador[caracter+c1])
+                    indiceColuna += 2
+                    continue
+                #-------------------------------------------------------------
+                
+                
+                #-------------------------------------------------------------
+                #testa se caractere é um separador
+                if caractere in self.separador:
+                    self.resolve(self.operador[caracter])
+                    indiceColuna += 1
+                    continue
+                #-------------------------------------------------------------
+                
+            indiceLinha += 1
+                
         while c != None:
 
             if self.comentario and c == "\n":
                 self.comentario = False
-                self.buffer = []
+                self.lexema = []
 
             if self.comentario:
                 c = self.arquivo.leProximoChar()
                 continue
 
-            if ''.join(self.buffer) == "//":
+            if ''.join(self.lexema) == "//":
                 self.comentario = True
 
-            if (c in self.separador or c in self.operador or c == " " or c == "\n") and (not self.comentario):
-                cadeia = ''.join(self.buffer)
+            if (not self.comentario) and (c in self.separador or c in self.operador or c == " " or c == "\n"):
+                cadeia = ''.join(self.lexema)
                 if cadeia is not '':
-                    print(''.join(self.buffer))
+                    print(''.join(self.lexema))
                     self.resolveToken(self.identificaToken(cadeia))
-                self.buffer = []
+                self.lexema = []
             else:
-                self.buffer.append(c)
+                self.lexema.append(c)
             c = self.arquivo.leProximoChar()
     
     def separador(self, char):
@@ -75,7 +217,7 @@ class AnalisadorLexico:
 
     def identificaToken(self, cadeiaDeCaracteres):
         # tipo = str(TipoToken[cadeiaDeCaracteres].value[1])
-        print(self.buffer)
+        print(self.lexema)
         if cadeiaDeCaracteres in self.operador:
             if cadeiaDeCaracteres == "=":
                 return Token("OPRecebe", "=")
